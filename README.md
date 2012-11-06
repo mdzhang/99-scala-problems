92ffbfbaf33b32790c5e05dea5133e0b2b396f71
Michelle D Zhang <zhang.michelle.d@gmail.com>
2012-11-06 17:32:47 -0600

    Initial commit, project scaffolding

---
+++ README.md
@@ -0,0 +1,1 @@
+Solutions and tests for Werner Hett's 99 problems in Scala (http://aperiodic.net/phil/scala/s-99/).
---
+++ src/test/scala/Example.scala
@@ -0,0 +1,13 @@
+package ninetynine
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ExampleSuite extends FunSuite {
+  test("P01"){
+    assert(true)
+  }
+}
